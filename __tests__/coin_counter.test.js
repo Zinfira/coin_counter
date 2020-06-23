a8f0b0542f1ed69cfedb92a9ad0256c3db55d1d3
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-06-22 18:20:33 -0700

    Pass all tests

--- __tests__/coin_counter.test.js
+++ __tests__/coin_counter.test.js
@@ -1,10 +1,49 @@
 import { Counter } from './../src/coin_counter.js';
 
 describe('Counter', () => {
+  let countCoin = new Counter();
+  let quarter = 0;
+  let dime = 0;
+  let nickel = 0;
+  let penny = 0;
 
   test('should return error message if argument is not a number', () => {
-    const counter = Counter("hey");
-    expect(counter).toBe("Invalid input")
+    const amount = "hey";
+    expect(countCoin.coinCounter(amount, quarter, dime, nickel, penny)).toBe("Invalid input")
   });
 
+  test('should return error message if argument is negative number', () => {
+    const amount = -2;
+    expect(countCoin.coinCounter(amount, quarter, dime, nickel, penny)).toBe("Invalid input")
+  });
+
+  test('should return zeros for all coins', () => {
+    const amount = 0;
+    expect(countCoin.coinCounter(amount)).toEqual([0, 0, 0, 0])
+  });
+
+  test('should return 1 quarter', () => {
+    const amount = 0.25;
+    expect(countCoin.coinCounter(amount)).toEqual([1, 0, 0, 0])
+  });
+
+  test('should return 1 dime', () => {
+    const amount = 0.1;
+    expect(countCoin.coinCounter(amount)).toEqual([0, 1, 0, 0])
+  });
+
+  test('should return 1 nickel', () => {
+    const amount = 0.05;
+    expect(countCoin.coinCounter(amount)).toEqual([0, 0, 1, 0])
+  });
+
+  test('should return 1 penny', () => {
+    const amount = 0.01;
+    expect(countCoin.coinCounter(amount)).toEqual([0, 0, 0, 1])
+  });
+
+  test('should return coins for 4.99', () => {
+    const amount = 4.99;
+    expect(countCoin.coinCounter(amount)).toEqual([19, 2, 0, 4])
+  });
 });
--- src/coin_counter.js
+++ src/coin_counter.js
@@ -1,4 +1,38 @@
-export function Counter(number) {
-  isNaN(number);
+export class Counter {
+  coinCounter(number) {
+  if (isNaN(number) || number < 0) {
     return "Invalid input";
+  }
+
+    const quarter = 0.25;
+    const dime = 0.1;
+    const nickel = 0.05;
+    const penny = 0.01;
+    const result = [0, 0, 0, 0];
+    return recurse(result);
+
+    function recurse(result) {
+      if (number === 0) {
+        return result;
+      }
+      if (number / quarter >= 1) {
+        result[0] = Math.trunc(number / quarter);
+        number = (number % quarter).toFixed(2);
+        return recurse(result);
+      }
+      if (number / dime >= 1) {
+        result[1] = Math.trunc(number / dime);
+        number = (number % dime).toFixed(2);
+        return recurse(result);
+      }
+      if (number / nickel >= 1) {
+        result[2] = Math.trunc(number / nickel);
+        number = (number % nickel).toFixed(2);
+        return recurse(result);
+      } else {
+        result[3] =Math.trunc(number / penny);
+        return result;
+      }
+    }
+  }
 }
